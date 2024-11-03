e4a09d9eea0e3bd5c5ae735551662d4b6b69443e
Kadir 🍁 <prostochasy@gmail.com>
2024-11-03 19:34:00 +0300

    Initialize Spring Boot project with basic structure and configuration

---
+++ src/main/java/com/kadir/starter/GalleristApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.kadir.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleristApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
